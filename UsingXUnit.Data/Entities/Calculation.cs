221cc8d3debb7b39d1f31c261c62f08ef731cf0f
qvarnstr0m <mqvarnstrom80@gmail.com>
2023-06-02 15:22:50 +0200

    Db logic

---
+++ UsingXUnit.Data/Entities/Calculation.cs
@@ -0,0 +1,9 @@
+namespace UsingXUnit.Data.Entities;
+
+public class Calculation
+{
+    public double firstNumber { get; set; }
+    public double secondNumber { get; set; }
+    public char operation { get; set; }
+    public double result { get; set; }
+}
